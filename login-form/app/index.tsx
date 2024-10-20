ad7d934862abacacb1e2bfa38f9ed87a5cf2ffb2
MUHAMMADFARHANHANIF <farhansmit0318@gmail.com>
2024-10-21 02:01:02 +0500

    more code

--- login-form/app/index.tsx
+++ login-form/app/index.tsx
@@ -2,10 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-     
-    >
-      <Text>Edit dit this screen.</Text>
+    <View>
+      <Text>Login Screen</Text>
     </View>
   );
 }
