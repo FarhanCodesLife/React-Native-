2bab1cae61c904edcc9cd21ded831826f10ab53e
MUHAMMADFARHANHANIF <farhansmit0318@gmail.com>
2024-10-28 00:33:00 +0500

    ok

--- 01-hello-world/app/index.tsx
+++ 01-hello-world/app/index.tsx
@@ -1,57 +1,11 @@
-import { useState, useEffect } from 'react';
-import {  Text, View, StyleSheet } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
-
-
-import * as Location from 'expo-location';
+import React from 'react';
+import MapView from 'react-native-maps';
+import { StyleSheet, View } from 'react-native';
 
 export default function App() {
-  const [location, setLocation] = useState<null | any>(null);
-  const [errorMsg, setErrorMsg] = useState<null | string>(null);
-
-  useEffect(() => {
-    (async () => {
-
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
-        return;
-      }
-
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
-      console.log(location);
-
-    })();
-  }, []);
-
-  // function hello(){
-  //   console.log('hello world')
-  // }
-  // hello()
-
-  let text = 'Waiting..';
-  if (errorMsg) {
-    text = errorMsg;
-  } else if (location) {
-    text = JSON.stringify(location);
-  }
-
   return (
     <View style={styles.container}>
-      <Text style={styles.paragraph}>{text}</Text>
-      {location && <MapView style={styles.map} initialRegion={{
-        latitude: location.coords.latitude,
-        longitude: location.coords.longitude,
-        latitudeDelta: 0.0001,
-        longitudeDelta: 0.0001,
-      }}> 
-      <Marker coordinate={{
-        latitude: location.coords.latitude,
-        longitude: location.coords.longitude,
-      }}/>
-
-      </MapView>}
+      <MapView style={styles.map} />
     </View>
   );
 }
@@ -59,16 +13,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-  paragraph: {
-    fontSize: 18,
-    textAlign: 'center',
   },
   map: {
     width: '100%',
-    height: '100%',
-  }
+    height: '100%',
+  },
 });
